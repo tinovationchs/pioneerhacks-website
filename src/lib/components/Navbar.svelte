3e911b089c5a691ed8cb6e42bd2c1dcac6c8cdb5
rk097 <78620631+rk097@users.noreply.github.com>
2023-08-11 18:21:42 -0700

    prelim page format
    only testing stuff

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<div id="nav" class="flex w-full justify-between pl-5 pr-5 pt-2 pb-2">
+    <div id="logo">
+        <a href="/"><img src="/pioneerhead.png" class="h-14" /></a> <!--TEMPORARY-->
+    </div>
+    <div id="nav-links" class="pt-3 text-neutral-50">
+        <a href="#schedule">Schedule</a>
+        <a href="#sponsors">Sponsors</a>
+        <a href="#FAQ">FAQ</a>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,29 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<div id="content" class="w-full h-screen">
+
+</div>
+
+<div id="schedule" class="w-full h-screen">
+  wake up 
+</div>
+
+<div id="sponsors" class="w-full h-screen">
+  Juniper echo3D
+</div>
+
+<div id="FAQ" class="w-full h-screen">
+  how do i make websites?
+</div>
+
 <h1 class="text-3xl font-bold underline">
   PioneerHacks
 </h1>
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.gray.100);
+
   }
 </style>
