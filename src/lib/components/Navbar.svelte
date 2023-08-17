52748092abdaab2df06dccceb7a6e3a416e42b1e
Daniel Cui <solunian@gmail.com>
2023-08-16 22:09:48 -0700

    wipe readme

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,11 +1,11 @@
-<div class="">
+<nav class="flex flex-row">
   <a href="/">
-    <img src="/pioneerhead.png" alt="pioneer head" />
+    <img src="/pioneerhead.png" alt="pioneer head" class="w-20" />
   </a>
 
   <div class="">
-    <a href="#schedule">Schedule</a>
+    <a href="#schedule" class=""> Schedule </a>
     <a href="#sponsors">Sponsors</a>
     <a href="#faq">FAQ</a>
   </div>
-</div>
+</nav>
