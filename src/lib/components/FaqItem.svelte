6878b36eb9f4404524e9eb2714746a9daa7159fe
Daniel Cui <solunian@gmail.com>
2023-10-03 23:41:21 -0700

    fix caret svg auto shrink to fit

--- src/lib/components/FaqItem.svelte
+++ src/lib/components/FaqItem.svelte
@@ -12,32 +12,34 @@
   on:click={() => {
     isOpen = !isOpen;
   }}>
-  <div class="flex flex-row justify-between">
-    <span>{q}</span>
+  <div class="flex flex-row justify-between gap-2">
+    <span class="">{q}</span>
 
-    {#if !isOpen}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="h-6 w-6"
-        in:fade>
-        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-      </svg>
-    {:else}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="h-6 w-6"
-        in:fade>
-        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
-      </svg>
-    {/if}
+    <span class="h-6 w-6">
+      {#if !isOpen}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="h-6 w-6"
+          in:fade>
+          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
+      {:else}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="h-6 w-6"
+          in:fade>
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+        </svg>
+      {/if}
+    </span>
   </div>
 
   {#if isOpen}
